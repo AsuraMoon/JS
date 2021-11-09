2b14657653ece63f14aad5e9762af8dd91437b5f
AsuraMoon <jufredenucci@gmx.fr>
2021-11-09 13:00:11 +0100

    Initial commit

---
+++ Opérateur.js
@@ -0,0 +1,43 @@
+var number = 5;
+var text = "text";
+var bool = false;
+console.log(Boolean(number));
+console.log(Boolean(bool));
+
+var number="";
+console.log(Boolean(number));
+
+var number=0;
+console.log(Boolean(number));
+
+var text= undefined;
+console.log(Boolean(text));
+
+var number= null
+console.log(Boolean(text))
+
+var text = NaN
+console.log(Boolean(text))
+
+var testArray=[]
+
+console.log(typeof
+    testArray);
+
+testArray[0]= 'Asu';
+testArray[1]= 23;
+testArray[2]= true;
+testArray[5]= false;
+
+console.log(testArray);
+
+var person = {};
+person.firstName="Julien"
+person.lastName="Fredenucci"
+person.age=23
+person.student=true
+
+console.log(typeof
+    person)
+
+console.log(person)
